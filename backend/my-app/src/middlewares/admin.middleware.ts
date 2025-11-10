37db5e5f645c61e81c4ebadd197747c673653ac5
benaya josua <benaya.josua@gmail.com>
2025-11-10 22:35:14 +0700

    feat : membuat middleware untuk admin, biar bisa lebih secure apakah dia seorang admin atau tidak

---
+++ backend/my-app/src/middlewares/admin.middleware.ts
@@ -0,0 +1,30 @@
+import type { Response, NextFunction } from "express";
+import { AuthRequest } from "./auth.middleware";
+
+export const requireAdmin = (req: AuthRequest, res: Response, next:NextFunction) => {
+    try {
+        
+        // check apakah ada require user
+        if(!req.user){
+            return res.status(401).json({
+                message: "unauthorized. Please login first"
+            })
+        }
+
+        // check apakah role user adalah superadmin ? 
+        if(req.user.role !== "superadmin"){
+            return res.status(403).json({
+                message:"access denied, admin onlu"
+            })
+        }
+
+        // jika role benar maka 
+        next()
+
+    } catch (error : any){
+        console.error("Admin middleware error : ", error)
+        return res.status(500).json({
+            message:"server error in admin middleware"
+        })
+    }
+}
