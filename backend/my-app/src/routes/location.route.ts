aedc716aad4fe35727d8de42ba2749795cf1faaf
benaya josua <benaya.josua@gmail.com>
2025-11-15 14:09:31 +0700

    feat : buat routing location

---
+++ backend/my-app/src/routes/location.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { requireAuth } from "../middlewares/auth.middleware";
+import { requireAdmin } from "../middlewares/admin.middleware";
+import { createLocation,deleteLocation, updateLocation, getAllLocation, getLocationById } from "../controllers/location.controller"
+
+const router = Router()
+
+// GET all - admin & user
+router.get("/", getAllLocation)
+// GET by Id - admin & user
+router.get("/:id", getLocationById)
+
+// CREATE - admin
+router.post("/", requireAuth, requireAdmin, createLocation)
+// UPDATE - admin 
+router.put("/:id", requireAuth, requireAdmin, updateLocation)
+// DELETE - admin 
+router.delete("/:id", requireAuth, requireAdmin, deleteLocation)
+
+export default router
