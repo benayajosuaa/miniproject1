1d426e0a2160bdb5230fea9b2dc97bda55a8a5fc
benaya josua <benaya.josua@gmail.com>
2025-11-16 00:59:34 +0700

    feat : buat route untuk CRUD brand

---
+++ backend/my-app/src/routes/brand.route.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {requireAuth} from "../middlewares/auth.middleware"
+import {requireAdmin} from "../middlewares/admin.middleware"
+import {createBrand, updateBrand, deleteBrand, getAllBrand, getBrandById} from "../controllers/brand.controller"
+
+
+const router = Router()
+
+// GET - admin & user
+router.get("/", getAllBrand)
+// GET by id - admin & user
+router.get("/:id", getBrandById)
+
+// CREATE - admin 
+router.post("/", requireAuth, requireAdmin, createBrand)
+// UPDATE - admin 
+router.post("/:id", requireAuth, requireAdmin, updateBrand)
+// DELETE - admin 
+router.delete("/:id", requireAuth, requireAdmin, deleteBrand)
+
+
+export default router
+
