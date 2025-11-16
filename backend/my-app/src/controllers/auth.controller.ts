54807f9f32a539b0747d7ded8c4b7109d075d901
benaya josua <benaya.josua@gmail.com>
2025-11-17 00:09:11 +0700

    feat : membuat controller untuk register user

--- backend/my-app/src/controllers/auth.controller.ts
+++ backend/my-app/src/controllers/auth.controller.ts
@@ -3,7 +3,91 @@ import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
 import prisma from "../lib/prisma";
 import { ApiError } from "../utils/apiError";
+import { use } from "react";
 
+// Register Controller
+export const registerController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {name, email, password} = req.body ?? {}
+
+        // inisiasi dari masing-masing variabel 
+        const nameString = name.toString().trim()
+        const emailString = email.toString().trim()
+        const passwordString = password.toString().trim()
+
+        // check kelengkapan name, email, password
+        if(!nameString || !emailString || !passwordString){
+            throw new ApiError(400, "Name, email, password are required")
+        }
+
+        // check format email
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(emailString)) {
+            throw new ApiError(400, "Invalid email format");
+        }
+
+        // untuk check apakah sudah ada user sebelumnya dengan check nama emailnya
+        const userExist = await prisma.user.findUnique({
+            where : {email: emailString}
+        })
+
+        if(userExist){
+            throw new ApiError(409, "Email already registered")
+        }
+
+        // hash password yang dibuat user
+        const hashedPassword = await bcrypt.hash(passwordString, 10)
+
+        // ambil jwt secret
+        const JWT_SECRET = process.env.JWT_SECRET
+        if(!JWT_SECRET){
+            throw new ApiError(500, "Server misconfigured")
+        }
+
+
+        // buat user baru 
+        const newUser = await prisma.user.create({
+            data : {
+                name: nameString,
+                email: emailString,
+                password: hashedPassword
+            }
+        })
+
+
+        // buat tokennya
+        const token = jwt.sign(
+            {id: newUser.id, email: newUser.email, role: newUser.role},
+            JWT_SECRET,
+            {expiresIn: "1d"}
+        )
+
+        // send response
+        return res.status(201).json({
+            status:"Success",
+            message:"User registered successfully",
+            data : {
+                token,
+                newUser: {
+                    id: newUser.id,
+                    name: newUser.name,
+                    email: newUser.email,
+                    role: newUser.role
+                }
+            }
+
+        })
+
+    } catch (error){
+        next (error)
+    }
+}
+
+
+
+
+
+// Login Controller
 export const loginController = async (req: Request, res: Response, next: NextFunction) => {
     try {   
         // ambil data pada frontend
@@ -70,3 +154,4 @@ export const loginController = async (req: Request, res: Response, next: NextFun
         next(error)
     }
 }
+
