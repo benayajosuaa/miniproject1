0e40736f275e5f207512f18772580910b729780c
benaya josua <benaya.josua@gmail.com>
2025-11-18 00:22:51 +0700

    feat : membuat routing untuk order

---
+++ backend/my-app/src/routes/order.route.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { createOrder, getAllOrder, getMyOrder, getOrderById, updateOrderStatus} from "../controllers/order.controller";
+
+import { requireAuth } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+
+// CREATE ORDER - user
+router.post("/", requireAuth, createOrder);
+
+// GET MY ORDERS - user
+router.get("/my-orders", requireAuth, getMyOrder);
+
+// GET ORDER BY ID - admin & user
+router.get("/:id", requireAuth, getOrderById);
+
+// GET ALL ORDERS - ADMIN 
+router.get("/", requireAuth, getAllOrder);
+
+// UPDATE ORDER STATUS - ADMIN 
+router.patch("/:id/status", requireAuth, updateOrderStatus);
+
+export default router;
