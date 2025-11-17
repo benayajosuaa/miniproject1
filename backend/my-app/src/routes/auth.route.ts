53bfea12beae2ce8780ed357ecf177ce5a253e67
benaya josua <benaya.josua@gmail.com>
2025-11-17 15:42:59 +0700

    add : tambah routing untuk register user

--- backend/my-app/src/routes/auth.route.ts
+++ backend/my-app/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { loginController } from "../controllers/auth.controller";
+import { loginController, registerController } from "../controllers/auth.controller";
 import { requireAuth, AuthRequest } from "../middlewares/auth.middleware";
 
 const router = Router()
 
 router.post("/login", loginController)
+router.post("/register", registerController)
 
 router.get("/me", requireAuth, (req: AuthRequest, res) => {
     res.status(200).json({
