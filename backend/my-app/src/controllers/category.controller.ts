38c90d01e7ce6e2b71584e872f55793b0d7b2a5b
benaya josua <benaya.josua@gmail.com>
2025-11-18 09:16:54 +0700

    feature : buat pagination untuk category

--- backend/my-app/src/controllers/category.controller.ts
+++ backend/my-app/src/controllers/category.controller.ts
@@ -11,9 +11,6 @@ export const createCategory = async (req: Request, res: Response, next:NextFunct
         // check apakah ada nama sebelumnya ? 
         if(!name || !name.trim()){
             throw new ApiError(400,"Category name is required")
-            // return res.status(400).json({
-            //     message:"Category name is required"
-            // })
         }
         // kalau udah pas, maka buat kategory baru 
         const newCategory = await prisma.category.create({
@@ -42,29 +39,12 @@ export const updateCategory = async (req: Request, res: Response, next:NextFunct
         // check apakah id ada atau tidak 
         if(isNaN(Number(id))){
             throw new ApiError(404,"Invalid category Id Format")
-            // return res.status(404).json({
-            //     message:"Invalid category ID format"
-            // })
         }
         // check ketersedian nama category itu
         if(!name || !name.trim()){
             throw new ApiError(400, "Category name is required")
-            // return res.status(400).json({
-            //     message:"Category name is required"
-            // })
         }
-        // // check ketersedian nama dan id
-        // const found = await prisma.category.findUnique({
-        //     where : {id:Number(id)}
-        // })
-        // // check apakah ada kategori 
-        // if(!found){
-        //     return res.status(404).json({
-        //         message: "category not found"
-        //     })
-        // }
 
-        // jika id ada maka, update lah id nya
         const updated = await prisma.category.update({
             where: {id: Number(id)},
             data: {name : name.trim()}
@@ -90,28 +70,13 @@ export const deleteCategory = async (req: Request, res: Response, next:NextFunct
         // check apakah id itu adalah suatu hal yang valid
         if(isNaN(Number(id))){
             throw new ApiError(400, "Invalid category Id Format")
-            // return res.status(404).json({
-            //     message:"Invalid category ID format"
-            // })
         }
-        // inisiasi status kalau dia ditemukan
-        // const found = await prisma.category.findUnique({
-        //     where : {id: Number(id)}
-        // })
-        // if (!found){
-        //     return res.status(404).json({
-        //         message:"Category not found"
-        //     })
-        // }
-        // jika ada product yang menggunakan category itu 
+
         const linkedProducts = await prisma.product.count({ 
             where : {category_id: Number(id)}
         })
         if (linkedProducts > 0 ){
             throw new ApiError(409,`cannot delete this category, because ${linkedProducts} product still linked to this category`)
-            // return res.status(409).json({
-            //     message:`cannot delete this category, because ${linkedProducts} product still linked to this category`
-            // })
         }
 
 
@@ -135,14 +100,51 @@ export const deleteCategory = async (req: Request, res: Response, next:NextFunct
 export const getAllCategory = async (req: Request, res: Response, next:NextFunction) => {
     try {
 
-        const categories = await prisma.category.findMany({
-            orderBy: {name: "asc"}
+        // buat pagination 
+        const page = Number(req.query.page) || 1
+        const limit = Number(req.query.limit) || 10
+        const search = req.query.search?.toString() || ""
+
+        const skip = (page - 1) * limit
+
+        const whereClause : any = {}
+        
+        if(search){
+            whereClause.name = {
+                contains: search,
+                mode: "insensitive"
+            }
+        }
+
+        const totalItems = await prisma.category.count({
+            where: whereClause
         })
+
+
+        const categories = await prisma.category.findMany({
+            where: whereClause,
+            skip,
+            orderBy: {name: "asc"},
+            take: limit,
+            include:{
+                categories: true
+            }
+        })
+
+        // hitung total semua 
+        const totalPages = Math.ceil(totalItems/limit)
+
 
         return res.status(200).json({
             status:"succes",
             message:"Success get all categories", 
-            categories
+            pagination: {
+                currentPage: page,
+                totalPages,
+                totalItems,
+                limit
+            },
+            data: categories
         })
 
     } catch(error){
@@ -159,9 +161,6 @@ export const getCategoryById = async (req: Request, res: Response, next:NextFunc
 
         if(isNaN(Number(id))){
             throw new ApiError(404,"Invalid category Id Format")
-            // return res.status(404).json({
-            //     message:"Invalid category ID format"
-            // })
         }
 
         const categoryById = await prisma.category.findUnique({
@@ -170,9 +169,6 @@ export const getCategoryById = async (req: Request, res: Response, next:NextFunc
 
         if(!categoryById){
             throw new ApiError(404, "Category not exist")
-            // return res.status(404).json({
-            //     message:"Category not exist"
-            // })
         }
 
         return res.status(200).json({
